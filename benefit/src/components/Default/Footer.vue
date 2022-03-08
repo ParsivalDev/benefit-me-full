ad6992a11efeec0dc57b36227e97869408caa19a
Kamil Barwiński <kamilb.kontakt@gmail.com>
2022-03-08 15:33:15 +0100

    Update Footer.vue

--- benefit/src/components/Default/Footer.vue
+++ benefit/src/components/Default/Footer.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="mt-auto h-[200px] bg-yellow w-full">
-    <h1>Footer</h1>
+  <div class="mt-auto max-h-[200px] relative w-full">
+    <svg class="absolute bottom-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#FFCC00" fill-opacity="1" d="M0,128L80,122.7C160,117,320,107,480,117.3C640,128,800,160,960,170.7C1120,181,1280,171,1360,165.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
+      </path>
+    </svg>
   </div>
 </template>
 
